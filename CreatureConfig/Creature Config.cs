c83dc981ec895017cf05dfbd2a3911c29123d4a0
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2023-04-18 22:39:15 +1000

    Creating Repo for CreatureConfig

---
+++ CreatureConfig/Creature Config.cs
@@ -0,0 +1,187 @@
+﻿using HarmonyLib;
+using System;
+using System.IO;
+using BepInEx.Logging;
+using SMLHelper.V2.Handlers;
+using static CreatureConfig.CreatureConfigPlugin;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace CreatureConfig
+{
+    [HarmonyPatch]
+    internal class CreatureConfig
+    {
+        static Dictionary<string, float> defaultDamageValues = new Dictionary<string, float>()
+        {
+            { "BiterDmg",7F }
+        };
+
+        /*[HarmonyPatch(typeof(Player), nameof(Player.Awake))]
+        [HarmonyPostfix]
+        public static void PostfixStart(Player __instance)
+        {
+            // Check to see if this is the player
+            if (__instance.GetType() == typeof(Player))
+            {
+                //Populate default damage values dictionary
+                defaultDamageValues.Add("BiterDmg",7F);
+            }
+        }*/
+
+        [HarmonyPatch(typeof(Creature), nameof(Creature.Start))]
+        [HarmonyPrefix]
+        public static void PrefixCreatureStart(Creature __instance)
+        {
+            #region Attempt 1; Several Nested If Statements
+            /*if (__instance.gameObject.GetComponent<CrabSnake>())
+            {
+                logger.Log(LogLevel.Info, $"Found Crabsnake; setting damage to {config.CrabsnakeDmg}");
+                if(__instance.gameObject.GetComponent<CrabsnakeMeleeAttack>())
+                {
+                    logger.Log(LogLevel.Info, "Found CrabsnakeMeleeAttack component");
+                    __instance.gameObject.GetComponent<CrabsnakeMeleeAttack>().biteDamage = 1f;
+                }
+            }
+            if (__instance.gameObject.GetComponent<Biter>())
+            {
+                logger.Log(LogLevel.Info, "Found Biter");
+                if (__instance.gameObject.GetComponent<MeleeAttack>())
+                {
+                    logger.Log(LogLevel.Info, $"Found MeleeAttack component; setting damage to {config.BiterDmg}");
+                    __instance.gameObject.GetComponent<MeleeAttack>().biteDamage = config.BiterDmg;
+                }
+            }*/
+            #endregion
+
+            #region Attempt 2; getting TechType and running the result through a switch case
+            TechType __techType = CraftData.GetTechType(__instance.gameObject);
+            //logger.Log(LogLevel.Info, $"Creature TechType = {__techType}");
+            logger.Log(LogLevel.Info, $"Creature TechType = {defaultDamageValues["BiterDmg"]}");
+
+            switch (config.DamagePreset)
+            {
+                //Default, apply individual changes
+                case 4:
+                    switch(__techType)
+                    {
+                        //Handle generic cases (just a MeleeAttack component; change biteDamage)
+                        case TechType.Biter:
+                            ChangeGenericMeleeAttack(__instance, config.BiterDmg);
+                            break;
+                        case TechType.Bleeder:
+                            break;
+                        case TechType.Shuttlebug: //Blood Crawler
+                            break;
+                        case TechType.BoneShark:
+                            ChangeGenericMeleeAttack(__instance, config.BonesharkDmg);
+                            break;
+                        case TechType.CaveCrawler:
+                            break;
+                        case TechType.CrabSquid:
+                            break;
+                        case TechType.LavaLizard:
+                            break;
+                        case TechType.Mesmer:
+                            break;
+                        case TechType.Sandshark:
+                            ChangeGenericMeleeAttack(__instance, config.SandSharkDmg);
+                            break;
+                        case TechType.Stalker:
+                            break;
+
+                        //Handle unique cases (has a unique MeleeAttack component, often with a grab animation and cinematic damage; change case by case)
+                        //This includes; Crabsnake, Ghost, Juvenile Emperor?, Reaper, Sea Dragon, Sea Trader, Ampeel (Shocker), Warper 
+                        case TechType.Crabsnake:
+                            logger.Log(LogLevel.Info, $"Found Crabsnake; setting damage to {config.CrabsnakeDmg}");
+                            __instance.gameObject.GetComponent<CrabsnakeMeleeAttack>().biteDamage = config.CrabsnakeDmg;
+                            break;
+                    }
+                    break;
+
+                //Sandbox, make all damage values 1
+                case 1:
+                    break;
+
+                //Sudden Death, make all damage values 1000?
+                case 7:
+                    break;
+
+                //Damage Presets 2,3,5,6, multiply default damage values by a percentage
+                default:
+                    break;
+            }
+            #endregion
+        }
+
+        public static void ChangeGenericMeleeAttack(Creature __instance, float __damageValue)
+        {
+            //DEBUG CODE; prints creature type and damage set
+            TechType __techType = CraftData.GetTechType(__instance.gameObject);
+            logger.Log(LogLevel.Info, $"Setting {__techType} biteDamage to {__damageValue}");
+
+            //Set biteDamage to new damage value
+            __instance.gameObject.GetComponent<MeleeAttack>().biteDamage = __damageValue;
+        }
+
+
+        [HarmonyPatch(typeof(LiveMixin), nameof(LiveMixin.TakeDamage))]
+        [HarmonyPrefix]
+        public static void PrefixTakeDamage(LiveMixin __instance)
+        {
+            //If all else fails; hook into dealer and see what they are and retroactively change the damage here?
+            //https://harmony.pardeike.net/articles/patching-injections.html
+        }
+    }
+
+    [Serializable]
+    public class SpawnData
+    {
+        public bool AlwaysRandomized = false;
+        public float ReaperSpawnIntensity = 3;
+        public float GhostSpawnIntensity = 3;
+        public float[][] ReaperCoords =
+        {
+            new float[]{ 120, -40, -568 }, //Grassy Plateaus, South
+            new float[]{ 1407, -190, 584 }, //Bulb Zone, East-North-East
+            new float[]{ 278, -175, 1398 }, //Mountains, North
+            new float[]{ -811, -240, -1240 }, //Grand Reef, South-South-West
+            new float[]{ -442, -132, -912 }, //Grand Reef, South
+            new float[]{ -310, -45, 92 }, //Kelp Forest, West
+            new float[]{ 190, -52, 477 }, //Kelp Forest, North
+            new float[]{ 500, -98, 318 }, //Mushroom Forest, East
+            new float[]{ 680, -85, 331 }, //Mushroom Forest, East
+            new float[]{ -172, -70, -781 }, //Crag Field, South
+            new float[]{ -692, -130, -725 }, //Sparse Reef, South-West
+            new float[]{ -745, -80, -1050 }, //Grand Reef, South-South-West (Under Floating Island)
+            new float[]{ -278, -30, -621 }, //Kelp Forest, South
+            new float[]{ -295, -45, -350 }, //Grassy Plateaus, South-West
+            new float[]{ -516, -110, 531 }, //Mushroom Forest, North-West
+            new float[]{ -815, -68, 316 }, //Mushroom Forest, West-North-West
+            new float[]{ -531, -60, -175 }, //Grassy Plateaus, West
+            new float[]{ -250, -142, 906 }, //Underwater Islands, North
+            new float[]{ -1122, -113, 710 }, //Mushroom Forest, North-West
+            new float[]{ -1190, -102, -527 }, //Sea Treader's Path, West-South-West
+            new float[]{ -754, -102, 1334 }, //Blood Kelp Zone, North-North-West
+            new float[]{ 432, -65, 690 }, //Kelp Forest, North-North-East
+            new float[]{ 383, -60, 40 } //Grassy Plateaus, East
+        };
+        public float[][] GhostCoordsAndType =
+        {
+            new float[]{ -284, -293, 1100, 1 }, //Adult, Underwater Islands, North
+            new float[]{ 1065, -211, 466, 1 }, //Adult, Bulb Zone, East-North-East
+            new float[]{ 876, -122, 881, 1 }, //Adult, Bulb Zone, North-East
+            new float[]{ -28, -318, 1296, 1 }, //Adult, Mountains, North
+            new float[]{ 10, -219, -220, 2 }, //Juvenile, Jellyshroom Cave, South
+            new float[]{ -396, -350, -925, 2 }, //Juvenile, Grand Reef, South
+            new float[]{ -958, -300, -540, 2 }, //Juvenile, Blood Kelp Trench, South-West
+            new float[]{ -988, -885, 400, 2 }, //Juvenile, Lost River, West-North-West (Tree Cove)
+            new float[]{ -695, -478, -993, 2 }, //Juvenile, Grand Reef, South-South-West (Degasi Base)
+            new float[]{ -618, -213, -82, 2 }, //Juvenile, Jellyshroom Cave, West
+            new float[]{ -34, -400, 926, 2 }, //Juvenile, Underwater Islands, North
+            new float[]{ -196, -436, 1056, 2 }, //Juvenile, Underwater Islands, North
+            new float[]{ 1443, -260, 883, 2 }, //Juvenile, Bulb Zone, North-East
+            new float[]{ 1075, -475, 944, 2 } //Juvenile, Mountains, North-East (Lost River Entrance)
+        };
+    }
+}
---
+++ CreatureConfig/CreatureConfigPlugin.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Options;
+using SMLHelper.V2.Options.Attributes;
+using UnityEngine;
+using static OVRHaptics;
+
+namespace CreatureConfig
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class CreatureConfigPlugin : BaseUnityPlugin
+    {
+        private const string myGUID = "com.jukebox.creatureconfig";
+        private const string pluginName = "Creature Config";
+        private const string versionString = "0.0.1";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ManualLogSource logger;
+
+        internal static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+        }
+    }
+
+    [Menu("Creature Config")]
+    public class Config : SMLHelper.V2.Json.ConfigFile
+    {
+        [Slider("Damage Presets", Min = 1F, Max = 7F, DefaultValue = 4F, Step = 1F, Id = "DamagePreset", 
+            Tooltip = "The damage multiplier preset you wish to use if you want to quickly change all damage values. \n" +
+            "Keep in mind that changes made to individual creatures below will not take effect unless you select preset 4, Default. \n" +
+            "1 = Sandbox, all enemies deal 1 damage \n" +
+            "2 = Very Easy, All enemies deal 50% damage \n" +
+            "3 = Easy, All enemies deal 75% damage \n" +
+            "4 = Default, All enemies deal 100% damage and any individual changes made below will take effect \n" +
+            "5 = Hard, All enemies deal 125% damage \n" +
+            "6 = Very Hard, All enemies deal 150% damage \n" +
+            "7 = Sudden Death, All enemies will kill the player in one hit, including vehicles; you have been warned"), 
+            OnChange(nameof(PresetChanged))]
+        public float DamagePreset = 1.0F;
+
+        #region Ampeel
+        [Slider("Ampeel Bite Damage", Min = 1F, Max = 100F, DefaultValue = 45F, Step = 1F, Id = "AmpeelBiteDmg"), OnChange(nameof(DamageChanged))]
+        public float AmpeelBiteDmg = 45F;
+        [Slider("Ampeel Shock Damage", Min = 1F, Max = 100F, DefaultValue = 15F, Step = 1F, Id = "AmpeelShockDmg"), OnChange(nameof(DamageChanged))]
+        public float AmpeelShockDmg = 15F;
+        #endregion
+
+        #region Biter
+        [Slider("Biter Damage", Min = 1F, Max = 100F, DefaultValue = 7F, Step = 1F, Id = "BiterDmg"), OnChange(nameof(DamageChanged))]
+        public float BiterDmg = 7F;
+        #endregion
+
+        #region Bleeder
+        [Slider("Bleeder Damage", Min = 1F, Max = 100F, DefaultValue = 5F, Step = 1F, Id = "BleederDmg"), OnChange(nameof(DamageChanged))]
+        public float BleederDmg = 5F;
+        #endregion
+
+        #region Blood Crawler
+        [Slider("Crabsnake Damage", Min = 1F, Max = 100F, DefaultValue = 5F, Step = 1F, Id = "BloodCrawlerDmg"), OnChange(nameof(DamageChanged))]
+        public float BloodCrawlerDmg = 5F;
+        #endregion
+
+        #region Boneshark
+        [Slider("Boneshark Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "BonesharkDmg"), OnChange(nameof(DamageChanged))]
+        public float BonesharkDmg = 30F;
+        #endregion
+
+        #region Cave Crawler
+        [Slider("Cave Crawler Damage", Min = 1F, Max = 100F, DefaultValue = 5F, Step = 1F, Id = "CaveCrawlerDmg"), OnChange(nameof(DamageChanged))]
+        public float CaveCrawlerDmg = 5F;
+        #endregion
+
+        #region Crabsnake
+        [Slider("Crabsnake Damage", Min = 1F, Max = 100F, DefaultValue = 35F, Step = 1F, Id = "CrabsnakeDmg"), OnChange(nameof(DamageChanged))]
+        public float CrabsnakeDmg = 35F;
+        #endregion
+
+        #region Crabsquid
+        [Slider("Crabsquid Damage", Min = 1F, Max = 100F, DefaultValue = 40F, Step = 1F, Id = "CrabsquidDmg"), OnChange(nameof(DamageChanged))]
+        public float CrabsquidDmg = 40F;
+        #endregion
+
+        /*#region Ghost Leviathan
+        [Slider("Crabsnake Grab Damage", Min = 1F, Max = 100F, DefaultValue = 35F, Step = 1F, Id = "CrabsnakeGrabDmg"), OnChange(nameof(SpawnIntensityChanged))]
+        public float CrabsnakeGrabDmg = 35F;
+        #endregion
+
+        #region Ghost Leviathan Juvenile
+        [Slider("Crabsnake Grab Damage", Min = 1F, Max = 100F, DefaultValue = 35F, Step = 1F, Id = "CrabsnakeGrabDmg"), OnChange(nameof(SpawnIntensityChanged))]
+        public float CrabsnakeGrabDmg = 35F;
+        #endregion*/
+
+        #region Lava Lizard
+        [Slider("Lava Lizard Bite Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "LavaLizardBiteDmg"), OnChange(nameof(DamageChanged))]
+        public float LavaLizardBiteDmg = 30F;
+        [Slider("Lava Lizard Spit Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "LavaLizardSpitDmg"), OnChange(nameof(DamageChanged))]
+        public float LavaLizardSpitDmg = 30F;
+        #endregion
+
+        #region Mesmer
+        [Slider("Mesmer Damage", Min = 1F, Max = 100F, DefaultValue = 35F, Step = 1F, Id = "MesmerDmg"), OnChange(nameof(DamageChanged))]
+        public float MesmerDmg = 35F;
+        #endregion
+
+        /*#region Reaper Leviathan
+        [Slider("Crabsnake Grab Damage", Min = 1F, Max = 100F, DefaultValue = 35F, Step = 1F, Id = "CrabsnakeGrabDmg"), OnChange(nameof(SpawnIntensityChanged))]
+        public float CrabsnakeGrabDmg = 35F;
+        #endregion*/
+
+        /*#region River Prowler
+        [Slider("Crabsnake Grab Damage", Min = 1F, Max = 100F, DefaultValue = 35F, Step = 1F, Id = "CrabsnakeGrabDmg"), OnChange(nameof(DamageChanged))]
+        public float CrabsnakeGrabDmg = 35F;
+        #endregion*/
+
+        #region Sand Shark
+        [Slider("Sand Shark Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "SandSharkDmg"), OnChange(nameof(DamageChanged))]
+        public float SandSharkDmg = 30F;
+        #endregion
+
+        /*#region Sea Dragon Leviathan
+        [Slider("Crabsnake Grab Damage", Min = 1F, Max = 100F, DefaultValue = 35F, Step = 1F, Id = "CrabsnakeGrabDmg"), OnChange(nameof(SpawnIntensityChanged))]
+        public float CrabsnakeGrabDmg = 35F;
+        #endregion*/
+
+        #region Stalker
+        [Slider("Stalker Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "StalkerDmg"), OnChange(nameof(DamageChanged))]
+        public float StalkerDmg = 30F;
+        #endregion
+
+        /*#region Warper
+        [Slider("Crabsnake Grab Damage", Min = 1F, Max = 100F, DefaultValue = 35F, Step = 1F, Id = "CrabsnakeGrabDmg"), OnChange(nameof(SpawnIntensityChanged))]
+        public float CrabsnakeGrabDmg = 35F;
+        #endregion*/
+
+        private void PresetChanged(SliderChangedEventArgs e)
+        {
+            //float __percent = e.Value / 100;
+            //BiterDmg = __percent * 7;
+            //CrabsnakeDmg = __percente * 35;
+
+            //THIS SHOULD ROUND AFTER THE PERCENTAGE MULTIPLICATION AND ROUND TO THE NEAREST INTEGER, RATHER THAN MULTIPLE BY 1 or 2 ALWAYS
+            BiterDmg = (int)(e.Value * 7);
+            CrabsnakeDmg = (int)(e.Value * 35);
+
+            //What I think I'm going to do is have this slider do nothing on its own, but instead in the patched method to actually change the values, it will check the preset
+            //if the preset is 4, it uses individual; if not, it goes through each creature and multiplies its damage by the given multiplication (so passing 7 * 0.5 for example)
+            //Need a list of default values for this to work though, so going to use a Dictionary, with the specific default value it's looking for being a string key in the dictionary
+            //For example, if we're looking for BonesharkDmg, the dictionary would have "BonesharkDmg" as a key athat would lead to a default value of 30
+        }
+
+        private void DamageChanged(SliderChangedEventArgs e)
+        {
+            switch(e.Id)
+            {
+                case "BiterDmg":
+                    BiterDmg = e.Value;
+                    break;
+                case "BonesharkDmg":
+                    BonesharkDmg = e.Value;
+                    break;
+                case "CrabsnakeDmg":
+                    CrabsnakeDmg = e.Value;
+                    break;
+            }
+        }
+    }
+}
