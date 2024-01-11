a7ba317497c4998ac71bf8eef1f50c8ce8e39b28
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2024-01-11 20:54:27 +1100

    Established Creature Config Health Project
    Created a new project for Creature Config Health and added basic references for what I am likely to be using,

---
+++ CreatureConfigHealth/CreatureConfigHealthPlugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Handlers;
+using Nautilus.Options.Attributes;
+
+namespace CreatureConfigHealth
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class CreatureConfigHealthPlugin : BaseUnityPlugin
+    {
+        private const string myGUID = "com.jukebox.creatureconfighealth";
+        private const string pluginName = "Creature Config - Health";
+        private const string versionString = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ManualLogSource logger;
+
+        internal static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+        }
+    }
+
+    [Menu("Creature Config - Health")]
+    public class Config : Nautilus.Json.ConfigFile
+    {
+        
+    }
+}
