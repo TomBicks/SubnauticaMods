1bfe0fb2c8f55b194d1ecfe3dd96d2d60cc0e0eb
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2024-03-02 17:43:44 +1100

    Created initial project with very basic code
    Created the project for the Immortal Snail mod, and added code to spawn a green coloured Crashfish; otherwise entirely the same.
    Want to see if I can change the emissive texture colours too.

---
+++ ImmortalSnail/ImmortalSnail.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Nautilus.Assets.PrefabTemplates;
+using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
+using static ImmortalSnail.ImmortalSnailPlugin;
+using UnityEngine;
+
+namespace ImmortalSnail
+{
+    [HarmonyPatch]
+    internal class ImmortalSnail
+    {
+        [HarmonyPatch(typeof(Player), nameof(Player.Awake))]
+        [HarmonyPostfix]
+        public static void PostfixPlayer(Player __instance)
+        {
+            var immortalSnail = new CustomPrefab("ImmortalSnail", "Immortal Snail", null);
+
+            // Creates a clone of the Crashfish prefab and colors it green, then set the new prefab as our Immortal Snail's game object.          
+            var immortalSnailPrefab = new CloneTemplate(immortalSnail.Info, TechType.Crash)
+            {
+                ModifyPrefab = prefab => prefab.GetComponentsInChildren<Renderer>().ForEach(r => r.materials.ForEach(m => m.color = Color.green))
+            };
+            immortalSnail.SetGameObject(immortalSnailPrefab);
+
+            //
+            immortalSnail.SetSpawns(new SpawnLocation(new UnityEngine.Vector3(0, -30, 0)));
+
+            //Register the Immortal Snail to the game
+            immortalSnail.Register();
+        }
+    }
+}
---
+++ ImmortalSnail/ImmortalSnailPlugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Handlers;
+using Nautilus.Options.Attributes;
+
+namespace ImmortalSnail
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    internal class ImmortalSnailPlugin : BaseUnityPlugin
+    {
+        private const string myGUID = "com.jukebox.immortalsnail";
+        private const string pluginName = "Immortal Snail";
+        private const string versionString = "0.0.1";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        internal static ManualLogSource logger { get; private set; }
+
+        internal static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+        }
+    }
+
+    [Menu("Immortal Snail")]
+    public class Config : Nautilus.Json.ConfigFile
+    {
+
+    }
+}
