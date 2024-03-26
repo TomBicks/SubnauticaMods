b6fb5617928a73b118e7407b333751a40c99752e
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2024-03-26 16:00:14 +1100

    Copied bomb prefab has working animation and collision
    Managed to figure out how the animation triggers work for the bomb prefab, so now when the player approaches one, it starts to open up and vibrate.
    Likewise, have made the collision more appropriate for the small bomb that it is.
    Aside from still needing to implement all the other behaviour, the only issue is that before spawning a cope of my prefab in, one will be on top of the original bomb in the display case until I spawn it in, in which case it 'vanishes'.

--- ImmortalSnail/ImmortalSnail.cs
+++ ImmortalSnail/ImmortalSnail.cs
@@ -48,13 +48,31 @@ namespace ImmortalSnail
             IPrefabRequest request = PrefabDatabase.GetPrefabForFilenameAsync("WorldEntities/Doodads/Precursor/Prison/Relics/Alien_relic_06.prefab");
             yield return request;
             request.TryGetPrefab(out GameObject originalPrefab);
+
             GameObject resultPrefab = Object.Instantiate(originalPrefab);
 
+            //Edit originalPrefab and then instantiate it?
+            //CapsuleCollider seems to specify the range at which the animation is triggered
+            //Thus, this should be the 'warning' radius, where the bomb starts animating and, if it gets much closer, explodes
+            resultPrefab.GetComponent<CapsuleCollider>().radius = 3F;
+            resultPrefab.GetComponent<CapsuleCollider>().height = 6F;
+
+            //Enable CapsuleCollider's trigger on collision with player, so it can open up when close enough
+            resultPrefab.GetComponent<CapsuleCollider>().isTrigger = true;
+
+            //Make the actual physical collision sphere to the size of the model (when expanded), roughly
+            resultPrefab.FindChild("colliders").GetComponent<SphereCollider>().radius = 0.2F;
+
+            //Make the bomb itself overall slightly bigger, to make it easier to spot approaching
+            //NOTE!! Specifically the core_02 is the object housing the model, so better I scale that up than the whole thing
+            resultPrefab.FindChild("alien_relic_02_core_ctrl").GetComponent<Transform>().localScale *=1.5F;
+
+
             var immortalSnail = new CustomPrefab("ImmortalSnail", "Immortal Snail", null);
-            var bombPrefab = new CloneTemplate(immortalSnail.Info, TechType.PrecursorPrisonArtifact6)
+            /*var bombPrefab = new CloneTemplate(immortalSnail.Info, TechType.PrecursorPrisonArtifact6)
             {
                 //ModifyPrefab = prefab => prefab.GetComponentsInChildren<Mesh>();
-            };
+            };*/
 
             immortalSnail.SetGameObject(resultPrefab);
 
@@ -62,6 +80,17 @@ namespace ImmortalSnail
 
             //Register the Immortal Snail to the game
             immortalSnail.Register();
+        }
+
+        [HarmonyPatch(typeof(PlayerTriggerAnimation), nameof(PlayerTriggerAnimation.OnTriggerEnter))]
+        [HarmonyPostfix]
+        public static void PostfixPlayerAnimationTrigger(Collider __instance)
+        {
+            ErrorMessage.AddMessage("Test");
+            if (__instance.gameObject.TryGetComponent<Player>(out Player player))
+            {
+                ErrorMessage.AddMessage($"Player collided {player}");
+            }
         }
     }
 }
