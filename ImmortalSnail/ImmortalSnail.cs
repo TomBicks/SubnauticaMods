6b3d0d9773d860814f6d823182dc4f679e90ee06
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2024-03-26 22:19:37 +1100

    Core can be made to glow green
    Managed to find the methods to access the glow strength and color of the core of the bomb.
    Will now attempt the same to all 8 of the outer sections; the "sliders".

--- ImmortalSnail/ImmortalSnail.cs
+++ ImmortalSnail/ImmortalSnail.cs
@@ -16,32 +16,12 @@ namespace ImmortalSnail
         [HarmonyPostfix]
         public static void PostfixPlayer(Player __instance)
         {
-            /*var immortalSnail = new CustomPrefab("ImmortalSnail", "Immortal Snail", null);
-
-            // Creates a clone of the Crashfish prefab and colors it green, then set the new prefab as our Immortal Snail's game object.          
-            var immortalSnailPrefab = new CloneTemplate(immortalSnail.Info, TechType.Crash)
-            {
-                ModifyPrefab = prefab => prefab.GetComponentsInChildren<Renderer>().ForEach(r => r.materials.ForEach(m => m.color = Color.green))
-            };
-            immortalSnail.SetGameObject(immortalSnailPrefab);
-
-            immortalSnail.SetSpawns(new SpawnLocation(new UnityEngine.Vector3(0, -30, 0)));
-
-            //Register the Immortal Snail to the game
-            immortalSnail.Register();*/
-
             CoroutineHost.StartCoroutine(GetBombPrefab());
-            //GetBombPrefab();
         }
 
         private static IEnumerator GetBombPrefab()
         {
             ErrorMessage.AddMessage("Registering Bomb");
-
-            //ERROR! Alien Bomb has no CraftData, so have to use PrefabDatabase instead of this
-            /*CoroutineTask<GameObject> task = CraftData.GetPrefabForTechTypeAsync(TechType.PrecursorPrisonArtifact6, true);
-            yield return task;
-            GameObject prefab = task.GetResult();*/
 
             //WorldEntities/Doodads/Precursor/Prison/Relics/Alien_relic_06.prefab
             //IPrefabRequest request = PrefabDatabase.GetPrefabAsync("3c5abaf7-b18e-4835-8282-874763343d57");
@@ -51,22 +31,8 @@ namespace ImmortalSnail
 
             GameObject resultPrefab = Object.Instantiate(originalPrefab);
 
-            //Edit originalPrefab and then instantiate it?
-            //CapsuleCollider seems to specify the range at which the animation is triggered
-            //Thus, this should be the 'warning' radius, where the bomb starts animating and, if it gets much closer, explodes
-            resultPrefab.GetComponent<CapsuleCollider>().radius = 3F;
-            resultPrefab.GetComponent<CapsuleCollider>().height = 6F;
-
-            //Enable CapsuleCollider's trigger on collision with player, so it can open up when close enough
-            resultPrefab.GetComponent<CapsuleCollider>().isTrigger = true;
-
-            //Make the actual physical collision sphere to the size of the model (when expanded), roughly
-            resultPrefab.FindChild("colliders").GetComponent<SphereCollider>().radius = 0.2F;
-
-            //Make the bomb itself overall slightly bigger, to make it easier to spot approaching
-            //NOTE!! Specifically the core_02 is the object housing the model, so better I scale that up than the whole thing
-            resultPrefab.FindChild("alien_relic_02_core_ctrl").GetComponent<Transform>().localScale *=1.5F;
-
+            //Apply all changes to the resulting prefab, preparing it for our purposes
+            EditBombPrefab(resultPrefab);
 
             var immortalSnail = new CustomPrefab("ImmortalSnail", "Immortal Snail", null);
             /*var bombPrefab = new CloneTemplate(immortalSnail.Info, TechType.PrecursorPrisonArtifact6)
@@ -76,21 +42,45 @@ namespace ImmortalSnail
 
             immortalSnail.SetGameObject(resultPrefab);
 
-            immortalSnail.SetSpawns(new SpawnLocation(new UnityEngine.Vector3(0, -30, 0)));
-
             //Register the Immortal Snail to the game
             immortalSnail.Register();
         }
 
         [HarmonyPatch(typeof(PlayerTriggerAnimation), nameof(PlayerTriggerAnimation.OnTriggerEnter))]
         [HarmonyPostfix]
-        public static void PostfixPlayerAnimationTrigger(Collider __instance)
+        private static void PostfixPlayerAnimationTrigger(Collider instance)
         {
-            ErrorMessage.AddMessage("Test");
-            if (__instance.gameObject.TryGetComponent<Player>(out Player player))
-            {
-                ErrorMessage.AddMessage($"Player collided {player}");
-            }
+            ErrorMessage.AddMessage($"{instance} collided with bomb.");
+        }
+
+        private static void EditBombPrefab(GameObject bombPrefab)
+        {
+            var bombColliders = bombPrefab.FindChild("colliders");
+            var bombCoreController = bombPrefab.FindChild("alien_relic_02_core_ctrl");
+
+            //Edit originalPrefab and then instantiate it?
+            //CapsuleCollider seems to specify the range at which the animation is triggered
+            //Thus, this should be the 'warning' radius, where the bomb starts animating and, if it gets much closer, explodes
+            bombPrefab.GetComponent<CapsuleCollider>().radius = 3F;
+            bombPrefab.GetComponent<CapsuleCollider>().height = 6F;
+
+            //Enable CapsuleCollider's trigger on collision with player, so it can open up when close enough
+            bombPrefab.GetComponent<CapsuleCollider>().isTrigger = true;
+
+            //Make the actual physical collision sphere to the size of the model (when expanded), roughly
+            bombColliders.GetComponent<SphereCollider>().radius = 0.2F;
+
+            //Make the bomb itself overall slightly bigger, to make it easier to spot approaching
+            //NOTE!! Specifically the core_02 is the object housing the model, so better I scale that up than the whole thing
+            bombCoreController.GetComponent<Transform>().localScale *= 1.5F;
+
+            //Access how to make the bomb glow (planning on having it glow more the closer it is to the player
+            //Blinding 100% glow for when it goes critical and explodes
+            Material coreMaterial = bombCoreController.FindChild("alien_relic_02_core").GetComponent<MeshRenderer>().material;
+
+            //Set core to be green at 20% glow
+            coreMaterial.SetColor(ShaderPropertyID._GlowColor, Color.green);
+            coreMaterial.SetFloat(ShaderPropertyID._GlowStrength, 20F);
         }
     }
 }
