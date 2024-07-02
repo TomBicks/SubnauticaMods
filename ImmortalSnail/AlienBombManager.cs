17b7dbc3c15089cc39ba8157d6b292be727fb5bc
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2024-07-02 17:55:01 +1000

    Added function to move and rotate the bomb
    Added functions to theoretically move and rotate the bomb towards the player, though they do not functioned yet.
    Also, Player.main can be used to reference the player object.

--- ImmortalSnail/AlienBombManager.cs
+++ ImmortalSnail/AlienBombManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HarmonyLib;
 using UnityEngine;
 
 namespace ImmortalSnail
@@ -9,8 +10,19 @@ namespace ImmortalSnail
         public static GameObject bombPrefab {  get; set; }
         public static GameObject bombObject { get; set; }
         public static Vector3 bombPosition { get; private set; }
+
+        private static float moveSpeed = 1.0f;
+        private static float turnSpeed = 1.0f;
+
         //This will be enabled, and remain enabled, only once; when the bomb is first released and tracking the player
         public static bool bombHunting { get; private set; }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.Awake))]
+        [HarmonyPostfix]
+        public static void PostfixPlayerUpdate(Player __instance)
+        {
+            UpdateBombRotation();
+        }
 
         public static void StartBombHunting()
         {
@@ -23,15 +35,24 @@ namespace ImmortalSnail
             bombObject = UnityEngine.Object.Instantiate(bombPrefab, bombPosition, Quaternion.identity);
         }
 
-        public static void DespawnBomb(GameObject alienBomb)
+        public static void DespawnBomb()
         {
             //Probbaly need a reference to the bomb somewhere; perhaps in this mod class? Or the savedata?
-            UnityEngine.Object.Destroy(alienBomb);
+            UnityEngine.Object.Destroy(bombObject);
         }
 
-        public static void UpdateBombPosition(GameObject alienBomb)
+        public static void UpdateBombPosition()
         {
-            bombPosition = alienBomb.gameObject.transform.position;
+            bombPosition = bombObject.gameObject.transform.position;
+        }
+
+        public static void UpdateBombRotation()
+        {
+            //https://discussions.unity.com/t/how-can-i-make-a-game-object-look-at-another-object/98932
+            //Determine the rotation required to face the player
+            var lookRotation = Quaternion.LookRotation((Player.main.transform.position - bombPosition).normalized);
+            //Slowly interpolate between where the bomb is currently facing and where it needs to face the player
+            bombObject.transform.rotation = Quaternion.Slerp(bombObject.transform.rotation, lookRotation, turnSpeed);
         }
     }
 }
