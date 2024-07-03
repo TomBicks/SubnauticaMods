4b3f9b88cea28b5ba05cb42e1b2d2f9f8d65ee0e
Thomas Bickley <tombicks@outlook.com>
2024-07-03 12:42:08 +1000

    Able to set a modifier to one decimal place
    Able to set the min and max size of the creature, randomly selected and remaining at one decimal place.
    We do this by multiplying the, for example, min 0.1 and max 1.5 into 1 and 15, then selecting a random integer between them, say 7.
    Then we divide back down by 7 and get the final size modifier of 0.7.

--- CreatureConfigSize/CreatureConfigSize.cs
+++ CreatureConfigSize/CreatureConfigSize.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using static CreatureConfigSize.CreatureConfigSizePlugin;
 using UnityEngine;
+using System;
 
 namespace CreatureConfigSize
 {
@@ -22,7 +23,11 @@ namespace CreatureConfigSize
             switch (techType)
             {
                 case TechType.ReaperLeviathan:
-                    var modifier = 4.0f;
+                    //NOTE!! If I want to deal with velocity, will have to deal with leash movement of leviathans too
+                    //NOTE!! Perhaps I can increase the speed of the leviathan, whilst inversely decreasing the turning speed of it, as it gets larger?
+                    //Probably no need to increase the turning speed of smaller leviathans; they seem to get buggy when it's put above 3
+                    //If they're faster larger, do I need to increase their leash distance a bit? Is that dangerous?
+                    var modifier = GetLargeSizeModifier();
                     logger.LogDebug($"Setting Reaper to {modifier}*size");
                     ErrorMessage.AddDebug($"Setting Reaper to {modifier}*size");
                     creature.transform.localScale = new Vector3(modifier, modifier, modifier);
@@ -30,12 +35,31 @@ namespace CreatureConfigSize
             }
         }
 
-        [HarmonyPatch(typeof(Player), nameof(Player.Awake))]
-        [HarmonyPrefix]
-        public static void PostfixPlayer(Player __instance)
+        private static float GetLargeSizeModifier()
         {
-            logger.LogDebug($"Testing");
-            ErrorMessage.AddDebug($"Testing");
+            //Return a random size between the min and max set for leviathan-size creatures
+            System.Random rand = new System.Random();
+            //Return an int value, between min and max values, both multiplied by 10
+            //We both by 10, so that we can get the numbers unbetween 1 & 2, or 10 and 20 here
+            //We'll divide them back down by 10 after we've gotten a random integer
+            //var modifierByTen = rand.Next((int)config.LargeCreatureMinSize * 10, (int)config.LargeCreatureMaxSize * 10);
+            var modifierByTen = rand.Next((int)(0.2f*10), (int)(4.5f*10));
+            ErrorMessage.AddDebug($"{modifierByTen}");
+            //Divide the modifier back down by 10, meaning we have a modifier to 1 decimal place.
+            var modifier = (float)modifierByTen / 10;
+            ErrorMessage.AddDebug($"{modifier}");
+
+
+            //This returns a value between 0.1 and 1
+            //var roundResult = Math.Round(rand.NextDouble(), 1, MidpointRounding.AwayFromZero);
+            //ErrorMessage.AddDebug($"{roundResult}");
+
+            return modifier;
+        }
+
+        private static void ChangeSize(GameObject creature)
+        {
+
         }
     }
 }
