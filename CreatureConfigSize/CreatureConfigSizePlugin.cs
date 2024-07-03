54856b5b749baa135edbffdc1615822b21662cc7
Thomas Bickley <tombicks@outlook.com>
2024-07-03 15:38:41 +1000

    Updated config tooltips and values

--- CreatureConfigSize/CreatureConfigSizePlugin.cs
+++ CreatureConfigSize/CreatureConfigSizePlugin.cs
@@ -31,23 +31,23 @@ namespace CreatureConfigSize
     [Menu("Creature Config - Size")]
     public class Config : Nautilus.Json.ConfigFile
     {
-        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "SmallCreatureMinSize",
-            Tooltip = "Minimum size modifier of small-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
-        public float SmallCreatureMinSize = 1F;
-        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "SmallCreatureMaxSize",
-            Tooltip = "Maximum size modifier of small-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
-        public float SmallCreatureMaxSize = 1F;
-        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "MedCreatureMinSize",
-            Tooltip = "Minimum size modifier of medium-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
-        public float MedCreatureMinSize = 1F;
-        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "MedCreatureMaxSize",
-            Tooltip = "Minimum size modifier of medium-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
-        public float MedCreatureMaxSize = 1F;
-        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "LargeCreatureMinSize",
-            Tooltip = "Minimum size modifier of leviathan-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
-        public float LargeCreatureMinSize = 1F;
-        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "LargeCreatureMaxSize",
-            Tooltip = "Maximum size modifier of leviathan-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
-        public float LargeCreatureMaxSize = 1F;
+        [Slider("Small Creature Minimum Size", Min = 0.1f, Max = 1f, DefaultValue = 1f, Step = 0.1f, Id = "SmallCreatureMinSize",
+            Tooltip = "Minimum size modifier of small-size creatures. For example, a value of '0.5' would make the minimum size half as small as the base size.")]
+        public float SmallCreatureMinSize = 1f;
+        [Slider("Small Creature Maximum Size", Min = 1f, Max = 4f, DefaultValue = 1f, Step = 0.1f, Id = "SmallCreatureMaxSize",
+            Tooltip = "Maximum size modifier of small-size creatures. For example, a value of '2' would make the maximum size twice as big as the base size.")]
+        public float SmallCreatureMaxSize = 1f;
+        [Slider("Medium Creature Minimum Size", Min = 0.1f, Max = 1f, DefaultValue = 1f, Step = 0.1f, Id = "MedCreatureMinSize",
+            Tooltip = "Minimum size modifier of medium-size creatures. For example, a value of '0.5' would make the minimum size half as small as the base size.")]
+        public float MedCreatureMinSize = 1f;
+        [Slider("Medium Creature Maximum Size", Min = 1f, Max = 4f, DefaultValue = 1f, Step = 0.1f, Id = "MedCreatureMaxSize",
+            Tooltip = "Minimum size modifier of medium-size creatures. For example, a value of '2' would make the maximum size twice as big as the base size.")]
+        public float MedCreatureMaxSize = 1f;
+        [Slider("Large Creature Minimum Size", Min = 0.1f, Max = 1f, DefaultValue = 1f, Step = 0.1f, Id = "LargeCreatureMinSize",
+            Tooltip = "Minimum size modifier of leviathan-size creatures. For example, a value of '0.5' would make the minimum size half as small as the base size.")]
+        public float LargeCreatureMinSize = 1f;
+        [Slider("Large Creature Maximum Size", Min = 1f, Max = 4f, DefaultValue = 1f, Step = 0.1f, Id = "LargeCreatureMaxSize",
+            Tooltip = "Maximum size modifier of leviathan-size creatures. For example, a value of '2' would make the maximum size twice as big as the base size.")]
+        public float LargeCreatureMaxSize = 1f;
     }
 }
