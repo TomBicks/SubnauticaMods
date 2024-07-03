f5ca9596e4df5881f3d7ed219e4c00e43d2d667e
Thomas Bickley <tombicks@outlook.com>
2024-07-03 11:17:56 +1000

    Created initial size config mod with code
    Created the initial project for the Creature Config Size mod, alongside code to specifically make a Reaper Leviathan 4 times larger.
    Obviously there are some collision concerns, but we'll see if that's even feasible to tackle.

---
+++ CreatureConfigSize/CreatureConfigSize.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using static CreatureConfigSize.CreatureConfigSizePlugin;
+using UnityEngine;
+
+namespace CreatureConfigSize
+{
+    [HarmonyPatch]
+    internal class CreatureConfigSize
+    {
+        [HarmonyPatch(typeof(Creature), nameof(Creature.Start))]
+        [HarmonyPrefix]
+        public static void PrefixCreatureStart(Creature __instance)
+        {
+            //Reference the gameObject Class directly, as none of the functionality uses the Creature class specifically
+            GameObject creature = __instance.gameObject;
+
+            TechType techType = CraftData.GetTechType(creature);
+
+            logger.LogDebug($"Setting size of TechType {techType}");
+            ErrorMessage.AddDebug($"Setting size of TechType  {techType}");
+
+            switch (techType)
+            {
+                case TechType.ReaperLeviathan:
+                    var modifier = 4.0f;
+                    logger.LogDebug($"Setting Reaper to {modifier}*size");
+                    ErrorMessage.AddDebug($"Setting Reaper to {modifier}*size");
+                    creature.transform.localScale = new Vector3(modifier, modifier, modifier);
+                    break;
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.Awake))]
+        [HarmonyPrefix]
+        public static void PostfixPlayer(Player __instance)
+        {
+            logger.LogDebug($"Testing");
+            ErrorMessage.AddDebug($"Testing");
+        }
+    }
+}
---
+++ CreatureConfigSize/CreatureConfigSizePlugin.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Handlers;
+using Nautilus.Options.Attributes;
+
+namespace CreatureConfigSize
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    [BepInDependency("com.snmodding.nautilus")]
+    internal class CreatureConfigSizePlugin : BaseUnityPlugin
+    {
+        private const string myGUID = "com.jukebox.creatureconfigsize";
+        private const string pluginName = "Creature Config - Size";
+        private const string versionString = "0.0.1";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        internal static ManualLogSource logger { get; private set; }
+
+        internal static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+        }
+    }
+
+    [Menu("Creature Config - Size")]
+    public class Config : Nautilus.Json.ConfigFile
+    {
+        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "SmallCreatureMinSize",
+            Tooltip = "Minimum size modifier of small-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
+        public float SmallCreatureMinSize = 1F;
+        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "SmallCreatureMaxSize",
+            Tooltip = "Maximum size modifier of small-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
+        public float SmallCreatureMaxSize = 1F;
+        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "MedCreatureMinSize",
+            Tooltip = "Minimum size modifier of medium-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
+        public float MedCreatureMinSize = 1F;
+        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "MedCreatureMaxSize",
+            Tooltip = "Minimum size modifier of medium-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
+        public float MedCreatureMaxSize = 1F;
+        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "LargeCreatureMinSize",
+            Tooltip = "Minimum size modifier of leviathan-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
+        public float LargeCreatureMinSize = 1F;
+        [Slider("Small Creature Minimum Size", Min = 1F, Max = 1F, DefaultValue = 1F, Step = 0.1F, Id = "LargeCreatureMaxSize",
+            Tooltip = "Maximum size modifier of leviathan-size creatures. For example, a value of '2' would make the minimum size two times as small as base.")]
+        public float LargeCreatureMaxSize = 1F;
+    }
+}
