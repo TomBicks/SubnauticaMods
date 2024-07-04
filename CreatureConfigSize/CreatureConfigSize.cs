ee48179d41c419c82b73ffccb286269d89d8af83
Thomas Bickley <tombicks@outlook.com>
2024-07-04 11:26:21 +1000

    Refined size function, using enum more
    Have moved more of my code into the function to get size class; it now gets modifier as well, and uses the enum for SizeClass to make sure it's very well legible.

--- CreatureConfigSize/CreatureConfigSize.cs
+++ CreatureConfigSize/CreatureConfigSize.cs
@@ -2,6 +2,7 @@
 using static CreatureConfigSize.CreatureConfigSizePlugin;
 using UnityEngine;
 using System;
+using static CreatureConfigSize.CreatureConfigSize;
 
 namespace CreatureConfigSize
 {
@@ -32,27 +33,10 @@ namespace CreatureConfigSize
             if(techType != TechType.None)
             {
                 logger.LogDebug($"Setting size of TechType {techType}");
-                ErrorMessage.AddDebug($"Setting size of TechType  {techType}");
+                ErrorMessage.AddDebug($"Setting size of TechType {techType}");
 
-                float modifier = 1.0f;
-
-                int sizeClass = GetCreatureSizeClass(techType);
-
-                switch(sizeClass)
-                {
-                    case (int)SizeClass.Small:
-                        modifier = GetSmallSizeModifier();
-                        break;
-                    case (int)SizeClass.Medium:
-                        modifier = GetMedSizeModifier();
-                        break;
-                    case (int)SizeClass.Large:
-                        modifier = GetLargeSizeModifier();
-                        break;
-                    default:
-                        logger.LogWarning($"Error! Could not retrieve size class for TechType {techType}!");
-                        break;
-                }
+                //Generate a modifier based on the creature's size class, retrieved from the CreatureSizeReference array
+                float modifier = GetCreatureSizeModifier(techType); ;
 
                 //NOTE!! If i use the reference array above, this switch statement would *only* be for unique changes made!
                 //NOTE!! Can also use this for any changes unique to the creature we need to make, if any; probably jsut for leviathans
@@ -71,18 +55,25 @@ namespace CreatureConfigSize
                 //Once we've retrieved the modifier, apply the change to size, by the modifier
                 ChangeSize(creature, modifier);
             }
+            else
+            {
+                logger.LogWarning($"Error! Creature {__instance.name} has no TechType!");
+            }
         }
 
         //Just an easier way to read the sizeClass of the creatures
-        public enum SizeClass { Small, Medium, Large }
+        public enum SizeClass { None, Small, Medium, Large }
 
-        private static int GetCreatureSizeClass(TechType techType)
+        private static float GetCreatureSizeModifier(TechType techType)
         {
-            //Return -1 if no size class can be found in the reference array
-            var size = -1;
+            //Return 0 if no size class can be found in the reference array
+            var sizeClass = SizeClass.None;
+
+            //Return a default modifier of 1 if no size class can be found in the reference array
+            var modifier = 1.0f;
 
             //Try to get the size class of the given TechType
-            for(var i = 0; i < 3; i++)
+            for (var i = 0; i < 3; i++)
             {
                 for(var j = 0; j < CreatureSizeReference[i].Length; j++)
                 {
@@ -90,15 +81,34 @@ namespace CreatureConfigSize
                     {
                         //We return i as the size class, as it refers to which of the 3 size arrays we found the TechType match in
                         //We use the SizeClass array in future references, for legibility
-                        size = i;
+                        sizeClass = (SizeClass)i;
                         break;
                     }
                 }
-                //No need to continue the loop if we've found our match
-                if (size != -1) { break; }
+                //If we found a size class, generate a modifier for it, then break from the loop
+                if (sizeClass != SizeClass.None)
+                {
+                    switch (sizeClass)
+                    {
+                        case SizeClass.Small:
+                            modifier = GetSmallSizeModifier();
+                            break;
+                        case SizeClass.Medium:
+                            modifier = GetMedSizeModifier();
+                            break;
+                        case SizeClass.Large:
+                            modifier = GetLargeSizeModifier();
+                            break;
+                    }
+                }
             }
 
-            return size;
+            if(sizeClass == SizeClass.None)
+            {
+                logger.LogWarning($"Error! Could not retrieve size class for TechType {techType}!");
+            }
+
+            return modifier;
         }
 
     private static float GetSmallSizeModifier()
@@ -108,7 +118,6 @@ namespace CreatureConfigSize
             //Return an int value, between min and max values, both multiplied by 10
             //We both by 10, so that we can get the numbers unbetween 1 & 2, or 10 and 20 here
             //We'll divide them back down by 10 after we've gotten a random integer
-            ErrorMessage.AddDebug($"Generating random size for small creature, between {config.SmallCreatureMinSize} and {config.SmallCreatureMaxSize}");
             var modifierByTen = rand.Next((int)config.SmallCreatureMinSize * 10, (int)config.SmallCreatureMaxSize * 10);
             //Divide the modifier back down by 10, meaning we have a modifier to 1 decimal place.
             var modifier = (float)modifierByTen / 10;
@@ -123,7 +132,6 @@ namespace CreatureConfigSize
             //Return an int value, between min and max values, both multiplied by 10
             //We both by 10, so that we can get the numbers unbetween 1 & 2, or 10 and 20 here
             //We'll divide them back down by 10 after we've gotten a random integer
-            ErrorMessage.AddDebug($"Generating random size for medium creature, between {config.MedCreatureMinSize} and {config.MedCreatureMaxSize}");
             var modifierByTen = rand.Next((int)config.MedCreatureMinSize * 10, (int)config.MedCreatureMaxSize * 10);
 
             //Divide the modifier back down by 10, meaning we have a modifier to 1 decimal place.
@@ -139,7 +147,6 @@ namespace CreatureConfigSize
             //Return an int value, between min and max values, both multiplied by 10
             //We both by 10, so that we can get the numbers unbetween 1 & 2, or 10 and 20 here
             //We'll divide them back down by 10 after we've gotten a random integer
-            ErrorMessage.AddDebug($"Generating random size for large creature, between {config.LargeCreatureMinSize} and {config.LargeCreatureMaxSize}");
             var modifierByTen = rand.Next((int)config.LargeCreatureMinSize * 10, (int)config.LargeCreatureMaxSize * 10);
 
             //Divide the modifier back down by 10, meaning we have a modifier to 1 decimal place.
