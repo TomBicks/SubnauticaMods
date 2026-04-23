797de9836ca2c262fb63786809e94391c382c2f0
Thomas Bickley <tombicks@outlook.com>
2026-04-24 02:14:49 +1000

    Cleaned DamageSystemPatch
    Streamlined the if statements and fixed the return value failing.
    
    Also renamed to CalculateArmourDamage, to better reflect what this function is doing; the class can describe what we're patching.
    
    Also, tested; the patch works in every respect, so no need to work on it further.

--- CreatureConfigDamage/Patches/DamageSystemPatch.cs
+++ CreatureConfigDamage/Patches/DamageSystemPatch.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HarmonyLib;
+﻿using HarmonyLib;
 using UnityEngine;
 
 namespace CreatureConfigDamage.Patches;
@@ -14,7 +9,7 @@ internal class DamageSystemPatch
     //Patch damage calculations, "ignoring" the reduction in damage from the reinforced diving suit, if a creature has been set by the user to ignore it.
     [HarmonyPatch(nameof(DamageSystem.CalculateDamage))]
     [HarmonyPostfix]
-    public static float PostfixCalculateDamage(float damage, DamageType type, GameObject target, GameObject dealer)
+    public static float CalculateArmourDamage(float damage, DamageType type, GameObject target, GameObject dealer)
     {
         if (dealer != null)
         {
@@ -36,16 +31,13 @@ internal class DamageSystemPatch
                 TechType dealerTechType = CraftData.GetTechType(dealer);
 
                 //If the player is wearing armour, and the creature is set to ignore it, remove the damage reduction
-                if (armourValue > 0 && Plugin.config.IgnoreArmour.ContainsKey(dealerTechType))
+                if (armourValue > 0 && Plugin.config.IgnoreArmour.TryGetValue(dealerTechType, out bool ignoring))
                 {
-                    if (Plugin.config.IgnoreArmour[dealerTechType])
+                    if (ignoring) //false if fails to find an entry, or if not ignoring armour; works either way
                     {
                         //Calculate what to multiply the reduced damage by to restore it back to full damage
                         float originalDamageMultiplier = 1 / (1 - armourValue);
-                        //TODO!! "Harmony003" problem???
-                        damage *= originalDamageMultiplier;
-
-                        return damage;
+                        return damage * originalDamageMultiplier;
                     }
                 }
             }
@@ -54,3 +46,13 @@ internal class DamageSystemPatch
         return damage; //Returns the damage value calculated at the end of the patched function, not the damage given at the start
     }
 }
+
+//CALCULATIONS FROM TESTING
+/* 80 - (80*0.52) = 38.4
+ * 80/38.4 = 2.083333333 = 1/0.48
+ * 80 - (80*0.4) = 48
+ * 80/48 = 1.666666666 = 1/0.6
+ * 80 - (80*0.12) = 70.4
+ * 80/70.4 = 1.136363636 = 1/0.88
+ * modifier = 1/(1-num2) where num2 is the % protection reinforced dive suit pieces give
+ */
