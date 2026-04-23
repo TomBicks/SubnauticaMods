2af61c537aec5424ca396796fdaf5d1fdf1aaffa
Thomas Bickley <tombicks@outlook.com>
2026-04-23 20:53:03 +1000

    Added damage default and tested gaspod prefab edit
    Added default damge values to their own file for retrieval, and tested whether editing the gaspod prefab would change the damage for all gaspods spawned, as Gasopods only have a reference to the prefab, not their own copy of it; it worked!

---
+++ CreatureConfigDamage/DamageDefaults.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace CreatureConfigDamage;
+
+internal class DamageDefaults
+{
+    public static readonly Dictionary<string, float> defaultDamageValues = new Dictionary<string, float>()
+    {
+        { "AmpeelBiteDmg",30F },
+        { "AmpeelShockDmg",15F },
+        { "AmpeelCyclopsDmg",50F },
+        { "BiterDmg",7F },
+        { "BleederDmg",5F },
+        { "BlighterDmg",7F },
+        { "BloodCrawlerDmg",5F },
+        { "BonesharkDmg",30F },
+        { "CaveCrawlerDmg",5F },
+        { "CrabsnakeDmg",35F },
+        { "CrabsquidDmg",40F },
+        { "CrashfishDmg",50F },
+        { "DroopingStingerDmg", 50F },
+        { "GasopodGasPodDmg",10F },
+        { "GhostLeviathanDmg",85F },
+        { "GhostLeviathanCyclopsDmg",250F },
+        { "GhostLeviathanJuvenileDmg",55F },
+        { "GhostLeviathanJuvenileCyclopsDmg",220F },
+        { "LavaLizardBiteDmg",30F },
+        { "LavaLizardLavaRockDmg",15F },
+        { "MesmerDmg",35F },
+        { "ReaperLeviathanDmg",80F },
+        { "ReaperLeviathanCyclopsDmg",220F },
+        { "RiverProwlerDmg",30F },
+        { "SandsharkDmg",30F },
+        { "SeaDragonLeviathanBiteDmg",300F },
+        { "SeaDragonLeviathanSwatDmg",70F },
+        { "SeaDragonLeviathanShoveDmg",250F },
+        { "SeaDragonLeviathanBurningChunkDmg",10F },
+        { "SeaDragonLeviathanLavaMeteorDmg",40F },
+        { "SeaTreaderLeviathanDmg",40F },
+        { "StalkerDmg", 30F },
+        { "TigerPlantDmg", 10F },
+        { "WarperClawDmg", 30F },
+        { "WarperWarpDmg", 10F }
+    };
+}
---
+++ CreatureConfigDamage/Patches/PlayerPatch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using HarmonyLib;
+using UnityEngine;
+using UWE;
+
+namespace CreatureConfigDamage.Patches;
+
+//DEBUG!! This will eventually be handled in the wait screen handler stuff
+[HarmonyPatch(typeof(Player))]
+internal class PlayerPatch : MonoBehaviour
+{
+    [HarmonyPatch(nameof(Player.Start))]
+    [HarmonyPostfix]
+    public static void PlayerStart()
+    {
+        CoroutineHost.StartCoroutine(TestPrefabStuff());
+    }
+
+    private static IEnumerator TestPrefabStuff()
+    {
+        ErrorMessage.AddError("TestPrefabStuff");
+        Plugin.Logger.LogError("TestPrefabStuff");
+        CoroutineTask<GameObject> task = CraftData.GetPrefabForTechTypeAsync(TechType.GasPod);
+        yield return task;
+
+        GameObject prefab = task.GetResult();
+        ErrorMessage.AddError($"{prefab}");
+        Plugin.Logger.LogError(prefab);
+        //NOTE!! Can confirm; this makes all gaspods from here on deal 1 damage per second, meaning it works, rather than patching into the gaspod itself
+        prefab.GetComponent<GasPod>().damagePerSecond = 1;
+
+        //Instantiate the prefab with a random rotation 2 meters in front of the player camera:
+        Instantiate(prefab, MainCamera.camera.transform.position + (MainCamera.camera.transform.forward * 2), UnityEngine.Random.rotation);
+        ErrorMessage.AddError("Spawning Test GasPod");
+        Plugin.Logger.LogError("Spawning Test GasPod");
+    }
+}
