959b62d6cb529bbbdb3acd9650d006c7d185bfd4
Thomas Bickley <tombicks@outlook.com>
2026-04-27 00:16:49 +1000

    Updated AttackInfo with config function
    AttackInfo class now has a field for a function which retrieves the config variable's value associated with that attack.
    
    AttackData has been updated to reflect this. For example, with a Biter, it's configValue=() => Plugin.config.BiterBite
    
    Fields required for the code to function have been given the required property also.
    
    Finally, The variables in the config class have been updated, to reflect the streamlines variables names given in AttackData for the key names. Plus, a few descriptions have been updated to reflect these new names also.

--- CreatureConfigDamage/AttackData.cs
+++ CreatureConfigDamage/AttackData.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CreatureConfigDamage;
 
@@ -7,91 +8,92 @@ internal class AttackData
     public static readonly List<(TechType, AttackInfo[])> creatureAttacks = new List<(TechType, AttackInfo[])>()
     {
         { (TechType.Shocker, new AttackInfo[] { //Ampeel
-            new AttackInfo { attackKey="AmpeelBite", defaultDamage=30f },
-            new AttackInfo { attackKey="AmpeelShock", defaultDamage=15f, isGenericAttack=false },
-            new AttackInfo { attackKey="AmpeelCyclopsShock", defaultDamage=50f, isGenericAttack=false }
+            new AttackInfo { attackKey="AmpeelBite", defaultDamage=30f, configValue=() => Plugin.config.AmpeelBite },
+            new AttackInfo { attackKey="AmpeelShock", defaultDamage=15f, configValue=() => Plugin.config.AmpeelShock, isGenericAttack=false },
+            new AttackInfo { attackKey="AmpeelCyclopsShock", defaultDamage=50f, configValue=() => Plugin.config.AmpeelCyclopsShock, isGenericAttack=false }
         })},
         { (TechType.Biter, new AttackInfo[] {
-            new AttackInfo { attackKey="BiterBite", defaultDamage=7f }
+            new AttackInfo { attackKey="BiterBite", defaultDamage=7f, configValue=() => Plugin.config.BiterBite }
         })},
         { (TechType.Bleeder, new AttackInfo[] {
-            new AttackInfo { attackKey="BleederSuck", defaultDamage=5f, isGenericAttack=false }
+            new AttackInfo { attackKey="BleederSuck", defaultDamage=5f, configValue=() => Plugin.config.BleederSuck, isGenericAttack=false }
         })},
         { (TechType.Blighter, new AttackInfo[] {
-            new AttackInfo { attackKey="BlighterBite", defaultDamage=7f }
+            new AttackInfo { attackKey="BlighterBite", defaultDamage=7f, configValue=() => Plugin.config.BlighterBite }
         })},
         { (TechType.Shuttlebug, new AttackInfo[] { //Blood Crawler
-            new AttackInfo { attackKey="BloodCrawlerBite", defaultDamage=5f }
+            new AttackInfo { attackKey="BloodCrawlerBite", defaultDamage=5f, configValue=() => Plugin.config.BloodCrawlerBite }
         })},
         { (TechType.BoneShark, new AttackInfo[] {
-            new AttackInfo { attackKey="BoneSharkBite", defaultDamage=30f }
+            new AttackInfo { attackKey="BoneSharkBite", defaultDamage=30f, configValue=() => Plugin.config.BoneSharkBite }
         })},
         { (TechType.CaveCrawler, new AttackInfo[] {
-            new AttackInfo { attackKey="CaveCrawlerBite", defaultDamage=5f }
+            new AttackInfo { attackKey="CaveCrawlerBite", defaultDamage=5f, configValue=() => Plugin.config.CaveCrawlerBite }
         })},
         { (TechType.Crabsnake, new AttackInfo[] {
-            new AttackInfo { attackKey="CrabsnakeBite", defaultDamage=35f, isGenericAttack=false }
+            new AttackInfo { attackKey="CrabsnakeBite", defaultDamage=35f, configValue=() => Plugin.config.CrabsnakeBite, isGenericAttack=false }
         })},
         { (TechType.CrabSquid, new AttackInfo[] {
-            new AttackInfo { attackKey="CrabSquidBite", defaultDamage=40f }
+            new AttackInfo { attackKey="CrabSquidBite", defaultDamage=40f, configValue=() => Plugin.config.CrabSquidBite }
         })},
         { (TechType.Crash, new AttackInfo[] { //Crashfish
-            new AttackInfo { attackKey="CrashfishExplosion", defaultDamage=50f, isGenericAttack=false }
+            new AttackInfo { attackKey="CrashfishExplosion", defaultDamage=50f, configValue=() => Plugin.config.CrashfishExplosion, isGenericAttack=false }
         })},
         { (TechType.GasPod, new AttackInfo[] { //Gasopod's Gas Pods (the damage is tied to the pods, not the creature itself)
-            new AttackInfo { attackKey="GasPodPoison", defaultDamage=10f, isGenericAttack=false }
+            new AttackInfo { attackKey="GasPodPoison", defaultDamage=10f, configValue=() => Plugin.config.GasPodPoison, isGenericAttack=false }
         })},
         { (TechType.GhostLeviathan, new AttackInfo[] {
-            new AttackInfo { attackKey="GhostBite", defaultDamage=85f, isGenericAttack=false },
-            new AttackInfo { attackKey="GhostCyclopsBite", defaultDamage=250f, isGenericAttack=false }
+            new AttackInfo { attackKey="GhostBite", defaultDamage=85f, configValue=() => Plugin.config.GhostBite, isGenericAttack=false },
+            new AttackInfo { attackKey="GhostCyclopsBite", defaultDamage=250f, configValue=() => Plugin.config.GhostCyclopsBite, isGenericAttack=false }
         })},
         { (TechType.GhostLeviathanJuvenile, new AttackInfo[] {
-            new AttackInfo { attackKey="GhostJuvBite", defaultDamage=55f, isGenericAttack=false },
-            new AttackInfo { attackKey="GhostJuvCyclopsBite", defaultDamage=220f, isGenericAttack=false }
+            new AttackInfo { attackKey="GhostJuvBite", defaultDamage=55f, configValue=() => Plugin.config.GhostJuvBite, isGenericAttack=false },
+            new AttackInfo { attackKey="GhostJuvCyclopsBite", defaultDamage=220f, configValue=() => Plugin.config.GhostJuvCyclopsBite, isGenericAttack=false }
         })},
         { (TechType.LavaLizard, new AttackInfo[] {
-            new AttackInfo { attackKey="LavaLizardBite", defaultDamage=30f, },
-            new AttackInfo { attackKey="LavaLizardLavaRock", defaultDamage=15f, isGenericAttack=false }
+            new AttackInfo { attackKey="LavaLizardBite", defaultDamage=30f, configValue=() => Plugin.config.LavaLizardBite },
+            new AttackInfo { attackKey="LavaLizardLavaRock", defaultDamage=15f, configValue=() => Plugin.config.LavaLizardLavaRock, isGenericAttack=false }
         })},
         { (TechType.Mesmer, new AttackInfo[] {
-            new AttackInfo { attackKey="MesmerBite", defaultDamage=35f }
+            new AttackInfo { attackKey="MesmerBite", defaultDamage=35f, configValue=() => Plugin.config.MesmerBite }
         })},
         { (TechType.ReaperLeviathan, new AttackInfo[] {
-            new AttackInfo { attackKey="ReaperBite", defaultDamage=80f, isGenericAttack=false },
-            new AttackInfo { attackKey="ReaperCyclopsBite", defaultDamage=220f, isGenericAttack=false }
+            new AttackInfo { attackKey="ReaperBite", defaultDamage=80f, configValue=() => Plugin.config.ReaperBite, isGenericAttack=false },
+            new AttackInfo { attackKey="ReaperCyclopsBite", defaultDamage=220f, configValue=() => Plugin.config.ReaperCyclopsBite, isGenericAttack=false }
         })},
         { (TechType.SpineEel, new AttackInfo[] { //River Prowler
-            new AttackInfo { attackKey="StalkerBite", defaultDamage=30f }
+            new AttackInfo { attackKey="RiverProwlerBite", defaultDamage=30f, configValue=() => Plugin.config.RiverProwlerBite }
         })},
         { (TechType.Sandshark, new AttackInfo[] {
-            new AttackInfo { attackKey="SandsharkBite", defaultDamage=30f }
+            new AttackInfo { attackKey="SandsharkBite", defaultDamage=30f, configValue=() => Plugin.config.SandsharkBite }
         })},
         { (TechType.SeaDragon, new AttackInfo[] { //TODO!! SeaDragon was accordingly broken, so need to test these later (most notably the projectiles)
-            new AttackInfo { attackKey="SeaDragonBite", defaultDamage=300f, isGenericAttack=false },
-            new AttackInfo { attackKey="SeaDragonSwat", defaultDamage=70f, isGenericAttack=false },
-            new AttackInfo { attackKey="SeaDragonShove", defaultDamage=250f, isGenericAttack=false },
-            new AttackInfo { attackKey="SeaDragonBurningChunk", defaultDamage=10f, isGenericAttack=false },
-            new AttackInfo { attackKey="SeaDragonLavaMeteor", defaultDamage=40f, isGenericAttack=false }
+            new AttackInfo { attackKey="SeaDragonBite", defaultDamage=300f, configValue=() => Plugin.config.SeaDragonBite, isGenericAttack=false },
+            new AttackInfo { attackKey="SeaDragonSwat", defaultDamage=70f, configValue=() => Plugin.config.SeaDragonSwat, isGenericAttack=false },
+            new AttackInfo { attackKey="SeaDragonShove", defaultDamage=250f, configValue=() => Plugin.config.SeaDragonShove, isGenericAttack=false },
+            new AttackInfo { attackKey="SeaDragonBurningChunk", defaultDamage=10f, configValue=() => Plugin.config.SeaDragonBurningChunk, isGenericAttack=false },
+            new AttackInfo { attackKey="SeaDragonLavaMeteor", defaultDamage=40f, configValue=() => Plugin.config.SeaDragonLavaMeteor, isGenericAttack=false }
         })},
         { (TechType.SeaTreader, new AttackInfo[] {
-            new AttackInfo { attackKey="SeaTreaderPeck", defaultDamage=40f, isGenericAttack=false }
-        })},
-        { (TechType.SpikePlant, new AttackInfo[] { //Tiger Plant
-            new AttackInfo { attackKey="TigerPlantThorn", defaultDamage=10f, isGenericAttack=false }
+            new AttackInfo { attackKey="SeaTreaderPeck", defaultDamage=40f, configValue=() => Plugin.config.SeaTreaderPeck, isGenericAttack=false }
         })},
         { (TechType.Stalker, new AttackInfo[] {
-            new AttackInfo { attackKey="StalkerBite", defaultDamage=30f }
+            new AttackInfo { attackKey="StalkerBite", defaultDamage=30f, configValue=() => Plugin.config.StalkerBite }
+        })},
+        { (TechType.SpikePlant, new AttackInfo[] { //Tiger Plant
+            new AttackInfo { attackKey="TigerPlantThorn", defaultDamage=10f, configValue=() => Plugin.config.TigerPlantThorn, isGenericAttack=false }
         })},
         { (TechType.Warper, new AttackInfo[] {
-            new AttackInfo { attackKey="WarperClaw", defaultDamage=30f, isGenericAttack=false },
-            new AttackInfo { attackKey="WarperWarpBall", defaultDamage=10f, isGenericAttack=false }
+            new AttackInfo { attackKey="WarperClaw", defaultDamage=30f, configValue=() => Plugin.config.WarperClaw, isGenericAttack=false },
+            new AttackInfo { attackKey="WarperWarpBall", defaultDamage=10f, configValue=() => Plugin.config.WarperWarpBall, isGenericAttack=false }
         })},
     };
 }
 
 internal class AttackInfo
 {
-    internal string attackKey; // The string used to identify the specific attack among possibly several (also, if the attack isn't generic, the key will be used to determine its implementation)
-    internal float defaultDamage; // The damage the attack deals by default (will be used to calculate damage changes)
+    internal required string attackKey; // The string used to identify the specific attack among possibly several (also, if the attack isn't generic, the key will be used to determine its implementation)
+    internal required float defaultDamage; // The damage the attack deals by default (will be used to calculate damage changes)
+    internal required Func<float> configValue; // Stores a function to retrieve the user-selected config value associated with the attack (REMEMBER!!, '() =>', or arrow function, is just a shorthand function)
     internal bool isGenericAttack = true; // Whether the attack is simply a MeleeAttack component, or requires more specifics to apply changes (true by default)
 }
--- CreatureConfigDamage/DamageConfig.cs
+++ CreatureConfigDamage/DamageConfig.cs
@@ -6,7 +6,7 @@ namespace CreatureConfigDamage;
 [Menu("Creature Config - Damage")]
 public class DamageConfig : Nautilus.Json.ConfigFile
 {
-    [Slider("Damage Presets", Min = 1F, Max = 8F, DefaultValue = 1F, Step = 1F, Id = "DamagePreset",
+    [Slider("Damage Presets", Min = 1f, Max = 8f, DefaultValue = 1f, Step = 1f, Id = "DamagePreset",
         Tooltip = "The damage multiplier preset you wish to use if you want to quickly change all damage values. \n" +
         "Keep in mind that changes made to individual creatures below will not take effect unless you select preset 1, Custom. \n" +
         "1 = Custom, any individual changes made below will take effect \n" +
@@ -17,7 +17,7 @@ public class DamageConfig : Nautilus.Json.ConfigFile
         "6 = Hard, All enemies deal 25% more damage \n" +
         "7 = Very Hard, All enemies deal 50% more damage \n" +
         "8 = Sudden Death, All enemies will kill the player in one hit, including vehicles (cyclops takes 2 hits); you have been warned")]
-    public float DamagePreset = 1.0F;
+    public float DamagePreset = 1.0f;
 
     //NOTE!! Seamoth has 300 HP, Prawn Suit has 600 HP, and Cyclops has 1500 HP. As such, some attacks designed for vehicles may do less damage than appears when compared to attacking players
 
@@ -25,180 +25,181 @@ public class DamageConfig : Nautilus.Json.ConfigFile
     //NOTE!! Do any of these sliders actually need the ID variable to function? Am I just wasting space and code?
     //Isn't that what the variable below is for?
     #region Ampeel
-    [Slider("Ampeel Bite Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "AmpeelBiteDmg",
+    [Slider("Ampeel Bite Damage", Min = 1f, Max = 100f, DefaultValue = 30f, Step = 1f, Id = "AmpeelBiteDmg",
         Tooltip = "Damage dealt by an Ampeel's bite.")]
-    public float AmpeelBiteDmg = 30F;
-    [Slider("Ampeel Shock Damage", Min = 1F, Max = 100F, DefaultValue = 15F, Step = 1F, Id = "AmpeelShockDmg",
+    public float AmpeelBite = 30f;
+    [Slider("Ampeel Shock Damage", Min = 1f, Max = 100f, DefaultValue = 15f, Step = 1f, Id = "AmpeelShockDmg",
         Tooltip = "Damage dealt by an Ampeel's electricity.")]
-    public float AmpeelShockDmg = 15F;
-    [Slider("Ampeel Cyclops Damage", Min = 5F, Max = 500F, DefaultValue = 50F, Step = 5F, Id = "AmpeelCyclopsDmg",
-        Tooltip = "Damage dealt by an Ampeel's bite to a Cyclops.")]
-    public float AmpeelCyclopsDmg = 50F;
+    public float AmpeelShock = 15f;
+    [Slider("Ampeel Cyclops Damage", Min = 5f, Max = 500f, DefaultValue = 50f, Step = 5f, Id = "AmpeelCyclopsShock",
+        Tooltip = "Damage dealt by an Ampeel's electricity to a Cyclops.")]
+    public float AmpeelCyclopsShock = 50f;
     #endregion
 
     #region Biter
-    [Slider("Biter Damage", Min = 1F, Max = 100F, DefaultValue = 7F, Step = 1F, Id = "BiterDmg",
+    [Slider("Biter Damage", Min = 1f, Max = 100f, DefaultValue = 7f, Step = 1f, Id = "BiterBite",
         Tooltip = "Damage dealt by a Biter's bite.")]
-    public float BiterDmg = 7F;
+    public float BiterBite = 7f;
     #endregion
 
     #region Bleeder
-    [Slider("Bleeder Damage", Min = 1F, Max = 100F, DefaultValue = 5F, Step = 1F, Id = "BleederDmg",
+    [Slider("Bleeder Damage", Min = 1f, Max = 100f, DefaultValue = 5f, Step = 1f, Id = "BleederSuck",
         Tooltip = "Damage dealt by a Bleeder's grab attack every few seconds.")]
-    public float BleederDmg = 5F;
+    public float BleederSuck = 5f;
     #endregion
 
     #region Blighter
-    [Slider("Blighter Damage", Min = 1F, Max = 100F, DefaultValue = 7F, Step = 1F, Id = "BlighterDmg",
+    [Slider("Blighter Damage", Min = 1f, Max = 100f, DefaultValue = 7f, Step = 1f, Id = "BlighterBite",
         Tooltip = "Damage dealt by a Blighter's bite.")]
-    public float BlighterDmg = 7F;
+    public float BlighterBite = 7f;
     #endregion
 
     #region Blood Crawler
-    [Slider("Blood Crawler Damage", Min = 1F, Max = 100F, DefaultValue = 5F, Step = 1F, Id = "BloodCrawlerDmg",
+    [Slider("Blood Crawler Damage", Min = 1f, Max = 100f, DefaultValue = 5f, Step = 1f, Id = "BloodCrawlerBite",
         Tooltip = "Damage dealt by a Blood Crawler's bite.")]
-    public float BloodCrawlerDmg = 5F;
+    public float BloodCrawlerBite = 5f;
     #endregion
 
     #region Boneshark
-    [Slider("Boneshark Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "BonesharkDmg",
+    [Slider("Boneshark Damage", Min = 1f, Max = 100f, DefaultValue = 30f, Step = 1f, Id = "BoneSharkBite",
         Tooltip = "Damage dealt by a Boneshark's bite.")]
-    public float BonesharkDmg = 30F;
+    public float BoneSharkBite = 30f;
     #endregion
 
     #region Cave Crawler
-    [Slider("Cave Crawler Damage", Min = 1F, Max = 100F, DefaultValue = 5F, Step = 1F, Id = "CaveCrawlerDmg",
+    [Slider("Cave Crawler Damage", Min = 1f, Max = 100f, DefaultValue = 5f, Step = 1f, Id = "CaveCrawlerBite",
         Tooltip = "Damage dealt by a Cave Crawler's bite.")]
-    public float CaveCrawlerDmg = 5F;
+    public float CaveCrawlerBite = 5f;
     #endregion
 
     #region Crabsnake
-    [Slider("Crabsnake Damage", Min = 1F, Max = 100F, DefaultValue = 35F, Step = 1F, Id = "CrabsnakeDmg",
+    [Slider("Crabsnake Damage", Min = 1f, Max = 100f, DefaultValue = 35f, Step = 1f, Id = "CrabsnakeBite",
         Tooltip = "Damage dealt by a Crabsnake's bite.")]
-    public float CrabsnakeDmg = 35F;
+    public float CrabsnakeBite = 35f;
     #endregion
 
     #region Crabsquid
-    [Slider("Crabsquid Damage", Min = 1F, Max = 100F, DefaultValue = 40F, Step = 1F, Id = "CrabsquidDmg",
+    [Slider("Crabsquid Damage", Min = 1f, Max = 100f, DefaultValue = 40f, Step = 1f, Id = "CrabSquidBite",
         Tooltip = "Damage dealt by a Crabsquid's pincers.")]
-    public float CrabsquidDmg = 40F;
+    public float CrabSquidBite = 40f;
     #endregion
 
     #region Crashfish
-    [Slider("Crashfish Damage", Min = 1F, Max = 100F, DefaultValue = 50F, Step = 1F, Id = "CrashfishDmg",
+    [Slider("Crashfish Damage", Min = 1f, Max = 100f, DefaultValue = 50f, Step = 1f, Id = "CrashfishExplosion",
         Tooltip = "Damage dealt by a Crashfish's explosion.")]
-    public float CrashfishDmg = 0F;
+    public float CrashfishExplosion = 50f;
     #endregion
 
-    #region Drooping Stinger
-    [Slider("Drooping Stinger Damage", Min = 1F, Max = 100F, DefaultValue = 50F, Step = 1F, Id = "DroopingStingerDmg",
+    //TODO!! Not a creature, nor does it have a TechType, so it's difficult to retrieve and change at the moment
+    /*#region Drooping Stinger
+    [Slider("Drooping Stinger Damage", Min = 1f, Max = 100f, DefaultValue = 50f, Step = 1f, Id = "DroopingStingerDmg",
         Tooltip = "Damage dealt by a Drooping Stinger's venom over a few seconds.")]
-    public float DroopingStingerDmg = 50F;
-    #endregion
+    public float DroopingStingerDmg = 50f;
+    #endregion*/
 
     #region Gasopod
-    [Slider("Gasopod Gaspod Damage", Min = 1F, Max = 100F, DefaultValue = 10F, Step = 1F, Id = "GasopodGasPodDmg",
-        Tooltip = "Damage dealt per second by a Gasopod's poison cloud.")]
-    public float GasopodGasPodDmg = 10F;
+    [Slider("Gasopod Gaspod Damage", Min = 1f, Max = 100f, DefaultValue = 10f, Step = 1f, Id = "GasPodPoison",
+        Tooltip = "Damage dealt per second by a Gasopod's Gas Pod's poison cloud.")]
+    public float GasPodPoison = 10f;
     #endregion
 
     #region Ghost Leviathan
-    [Slider("Ghost Leviathan Damage", Min = 1F, Max = 100F, DefaultValue = 85F, Step = 1F, Id = "GhostLeviathanDmg",
+    [Slider("Ghost Leviathan Damage", Min = 1f, Max = 100f, DefaultValue = 85f, Step = 1f, Id = "GhostBite",
         Tooltip = "Damage dealt by a Ghost Leviathan's bite.")]
-    public float GhostLeviathanDmg = 85F;
-    [Slider("Ghost Leviathan Cyclops Damage", Min = 5F, Max = 500F, DefaultValue = 250F, Step = 5F, Id = "GhostLeviathanCyclopsDmg",
+    public float GhostBite = 85f;
+    [Slider("Ghost Leviathan Cyclops Damage", Min = 5f, Max = 500f, DefaultValue = 250f, Step = 5f, Id = "GhostCyclopsBite",
         Tooltip = "Damage dealt by a Ghost Leviathan's bite to a Cyclops.")]
-    public float GhostLeviathanCyclopsDmg = 250F;
+    public float GhostCyclopsBite = 250f;
     #endregion
 
     #region Ghost Leviathan Juvenile
-    [Slider("Ghost Leviathan Juv. Damage", Min = 1F, Max = 100F, DefaultValue = 55F, Step = 1F, Id = "GhostLeviathanJuvenileDmg",
+    [Slider("Ghost Leviathan Juv. Damage", Min = 1f, Max = 100f, DefaultValue = 55f, Step = 1f, Id = "GhostJuvBite",
         Tooltip = "Damage dealt by a Juvenile Ghost Leviathan's bite.")]
-    public float GhostLeviathanJuvenileDmg = 55F;
-    [Slider("Ghost Leviathan Juv. Cyclops Damage", Min = 5F, Max = 500F, DefaultValue = 220F, Step = 5F, Id = "GhostLeviathanJuvenileCyclopsDmg",
+    public float GhostJuvBite = 55f;
+    [Slider("Ghost Leviathan Juv. Cyclops Damage", Min = 5f, Max = 500f, DefaultValue = 220f, Step = 5f, Id = "GhostJuvCyclopsBite",
         Tooltip = "Damage dealt by a Juvenile Ghost Leviathan's bite to a Cyclops.")]
-    public float GhostLeviathanJuvenileCyclopsDmg = 220F;
+    public float GhostJuvCyclopsBite = 220f;
     #endregion
 
     #region Lava Lizard
-    [Slider("Lava Lizard Bite Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "LavaLizardBiteDmg",
+    [Slider("Lava Lizard Bite Damage", Min = 1f, Max = 100f, DefaultValue = 30f, Step = 1f, Id = "LavaLizardBite",
         Tooltip = "Damage dealt by a Lava Lizard's bite.")]
-    public float LavaLizardBiteDmg = 30F;
-    [Slider("Lava Lizard Lava Rock Damage", Min = 1F, Max = 100F, DefaultValue = 15F, Step = 1F, Id = "LavaLizardLavaRockDmg",
+    public float LavaLizardBite = 30f;
+    [Slider("Lava Lizard Lava Rock Damage", Min = 1f, Max = 100f, DefaultValue = 15f, Step = 1f, Id = "LavaLizardLavaRock",
         Tooltip = "Damage dealt by a Lava Lizard's lava rock projectile.")]
-    public float LavaLizardLavaRockDmg = 15F;
+    public float LavaLizardLavaRock = 15f;
     #endregion
 
     #region Mesmer
-    [Slider("Mesmer Damage", Min = 1F, Max = 100F, DefaultValue = 35F, Step = 1F, Id = "MesmerDmg",
+    [Slider("Mesmer Damage", Min = 1f, Max = 100f, DefaultValue = 35f, Step = 1f, Id = "MesmerBite",
         Tooltip = "Damage dealt by a Mesmer's bite.")]
-    public float MesmerDmg = 35F;
+    public float MesmerBite = 35f;
     #endregion
 
     #region Reaper Leviathan
-    [Slider("Reaper Damage", Min = 1F, Max = 100F, DefaultValue = 80F, Step = 1F, Id = "ReaperLeviathanDmg",
+    [Slider("Reaper Damage", Min = 1f, Max = 100f, DefaultValue = 80f, Step = 1f, Id = "ReaperBite",
         Tooltip = "Damage dealt by a Reaper Leviathan's bite.")]
-    public float ReaperLeviathanDmg = 80F;
-    [Slider("Reaper Cyclops Damage", Min = 5F, Max = 500F, DefaultValue = 220F, Step = 5F, Id = "ReaperLeviathanCyclopsDmg",
+    public float ReaperBite = 80f;
+    [Slider("Reaper Cyclops Damage", Min = 5f, Max = 500f, DefaultValue = 220f, Step = 5f, Id = "ReaperCyclopsBite",
         Tooltip = "Damage dealt by a Reaper Leviathan's bite to a Cyclops.")]
-    public float ReaperLeviathanCyclopsDmg = 220F;
+    public float ReaperCyclopsBite = 220f;
     #endregion
 
     #region River Prowler
-    [Slider("River Prowler Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "RiverProwlerDmg",
+    [Slider("River Prowler Damage", Min = 1f, Max = 100f, DefaultValue = 30f, Step = 1f, Id = "RiverProwlerBite",
         Tooltip = "Damage dealt by a River Prowler's bite.")]
-    public float RiverProwlerDmg = 30F;
-    #endregion
-
-    #region Tiger Plant
-    [Slider("Tiger Plant Damage", Min = 1F, Max = 100F, DefaultValue = 10F, Step = 1F, Id = "TigerPlantDmg",
-        Tooltip = "Damage dealt by a Tiger Plant's barb projectile.")]
-    public float TigerPlantDmg = 10F;
+    public float RiverProwlerBite = 30f;
     #endregion
 
     #region Sand Shark
-    [Slider("Sand Shark Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "SandSharkDmg",
+    [Slider("Sandshark Damage", Min = 1f, Max = 100f, DefaultValue = 30f, Step = 1f, Id = "SandsharkBite",
         Tooltip = "Damage dealt by a Sandshark's bite.")]
-    public float SandSharkDmg = 30F;
+    public float SandsharkBite = 30f;
     #endregion
 
     #region Sea Dragon Leviathan
-    [Slider("Sea Dragon Bite Damage", Min = 5F, Max = 500F, DefaultValue = 300F, Step = 5F, Id = "SeaDragonLeviathanBiteDmg",
+    [Slider("Sea Dragon Bite Damage", Min = 5f, Max = 500f, DefaultValue = 300f, Step = 5f, Id = "SeaDragonBite",
         Tooltip = "Damage dealt by a Sea Dragon Leviathan's bite.")]
-    public float SeaDragonLeviathanBiteDmg = 300F;
-    [Slider("Sea Dragon Swat Damage", Min = 1F, Max = 100F, DefaultValue = 70F, Step = 1F, Id = "SeaDragonLeviathanSwatDmg",
+    public float SeaDragonBite = 300f;
+    [Slider("Sea Dragon Swat Damage", Min = 1f, Max = 100f, DefaultValue = 70f, Step = 1f, Id = "SeaDragonSwat",
         Tooltip = "Damage dealt by a Sea Dragon Leviathan's swat.")]
-    public float SeaDragonLeviathanSwatDmg = 70F;
-    [Slider("Sea Dragon Shove Damage", Min = 5F, Max = 500F, DefaultValue = 250F, Step = 5F, Id = "SeaDragonLeviathanShoveDmg",
+    public float SeaDragonSwat = 70f;
+    [Slider("Sea Dragon Shove Damage", Min = 5f, Max = 500f, DefaultValue = 250f, Step = 5f, Id = "SeaDragonShove",
         Tooltip = "Damage dealt by a Sea Dragon Leviathan shoving a Cyclops.")]
-    public float SeaDragonLeviathanShoveDmg = 250F;
-    [Slider("Sea Dragon Burning Chunk Damage", Min = 1F, Max = 100F, DefaultValue = 10F, Step = 1F, Id = "SeaDragonLeviathanBurningChunkDmg",
+    public float SeaDragonShove = 250f;
+    [Slider("Sea Dragon Burning Chunk Damage", Min = 1f, Max = 100f, DefaultValue = 10f, Step = 1f, Id = "SeaDragonBurningChunk",
         Tooltip = "Damage dealt by a Sea Dragon Leviathan's burning chunk projectile volley.")]
-    public float SeaDragonLeviathanBurningChunkDmg = 10F;
-    [Slider("Sea Dragon Lava Meteor Damage", Min = 1F, Max = 100F, DefaultValue = 40F, Step = 1F, Id = "SeaDragonLeviathanLavaMeteorDmg",
+    public float SeaDragonBurningChunk = 10f;
+    [Slider("Sea Dragon Lava Meteor Damage", Min = 1f, Max = 100f, DefaultValue = 40f, Step = 1f, Id = "SeaDragonLavaMeteor",
         Tooltip = "Damage dealt by a Sea Dragon Leviathan's lava meteor projectile.")]
-    public float SeaDragonLeviathanLavaMeteorDmg = 40F;
+    public float SeaDragonLavaMeteor = 40f;
     #endregion
 
     #region Sea Treader Leviathan
-    [Slider("Sea Treader Damage", Min = 1F, Max = 100F, DefaultValue = 40F, Step = 1F, Id = "SeaTreaderLeviathanDmg",
+    [Slider("Sea Treader Damage", Min = 1f, Max = 100f, DefaultValue = 40f, Step = 1f, Id = "SeaTreaderPeck",
         Tooltip = "Damage dealt by a Sea Treader Leviathan's peck.")]
-    public float SeaTreaderLeviathanDmg = 40F;
+    public float SeaTreaderPeck = 40f;
     #endregion
 
     #region Stalker
-    [Slider("Stalker Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "StalkerDmg",
+    [Slider("Stalker Damage", Min = 1f, Max = 100f, DefaultValue = 30f, Step = 1f, Id = "StalkerBite",
         Tooltip = "Damage dealt by a Stalker's bite.")]
-    public float StalkerDmg = 30F;
+    public float StalkerBite = 30f;
+    #endregion
+
+    #region Tiger Plant
+    [Slider("Tiger Plant Damage", Min = 1f, Max = 100f, DefaultValue = 10f, Step = 1f, Id = "TigerPlantThorn",
+        Tooltip = "Damage dealt by a Tiger Plant's thorn projectile.")]
+    public float TigerPlantThorn = 10f;
     #endregion
 
     #region Warper
-    [Slider("Warper Claw Damage", Min = 1F, Max = 100F, DefaultValue = 30F, Step = 1F, Id = "WarperClawDmg",
+    [Slider("Warper Claw Damage", Min = 1f, Max = 100f, DefaultValue = 30f, Step = 1f, Id = "WarperClaw",
         Tooltip = "Damage dealt by a Warper's claws. Note that the damage is multiplied by the player's infection level, capping at 4x.")]
-    public float WarperClawDmg = 30F; //Damage increases based on infection level??? Starts at 23??? But biteDamge is 30??? LiveMixin with InfectionMixin to calcualte damage???
-    [Slider("Warper Warp Damage", Min = 1F, Max = 100F, DefaultValue = 10F, Step = 1F, Id = "WarperWarpDmg",
+    public float WarperClaw = 30f; //TODO!!Damage increases based on infection level??? Starts at 23??? But biteDamge is 30??? LiveMixin with InfectionMixin to calcualte damage???
+    [Slider("Warper Warp Damage", Min = 1f, Max = 100f, DefaultValue = 10f, Step = 1f, Id = "WarperWarpBall",
         Tooltip = "Damage dealt by a Warper's teleport projectile.")]
-    public float WarperWarpDmg = 10F;
+    public float WarperWarpBall = 10f;
     #endregion
     #endregion
 
